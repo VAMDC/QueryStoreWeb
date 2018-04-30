5b5a2cc626c1b7c8ab3ef8f030cfe4578c7e5186
nicolas <nicolas.moreau@obspm.fr>
2018-04-30 11:04:59 +0200

    Update service url

--- web/javascript/service_param.js
+++ web/javascript/service_param.js
@@ -1,4 +1,7 @@
 //const SERVICE_URL = "https://querystore.vamdc.eu/";
 const SERVICE_URL = "http://vm-calc-lerma01.obspm.fr/QueryStore/";
 const SITE_URL = 'http://lerma-m-nm2.obspm.fr/~nmoreau/query_store/';
-const SUBMIT_FOR_DOI_URL="http://vm-vamdc-querystore.obspm.fr/zenodo/submit?uuid="
+//dev
+const SUBMIT_FOR_DOI_URL="http://vm-vamdc-querystore.obspm.fr/zenododev/submit?uuid="
+//prod
+//const SUBMIT_FOR_DOI_URL="http://vm-vamdc-querystore.obspm.fr/zenodo/submit?uuid="
